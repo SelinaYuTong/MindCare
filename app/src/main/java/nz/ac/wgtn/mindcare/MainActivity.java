520ccd982ed1ca4b2f3051f44d348f0652d978b9
shingteha <Tahiti2021>
2021-10-20 00:12:04 +1300

    Minor updates, added content for Signup activity and changed images to buttons in Main activity class.

--- app/src/main/java/nz/ac/wgtn/mindcare/MainActivity.java
+++ app/src/main/java/nz/ac/wgtn/mindcare/MainActivity.java
@@ -19,4 +19,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, IntroActivity.class);
         startActivity(intent);
     }
+
+    public void loginBtnClick(View view){
+        Intent intent = new Intent(this, Signup.class);
+        startActivity(intent);
+    }
 }
