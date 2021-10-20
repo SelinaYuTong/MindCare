eddc1e22328c5ff61cc4bba178218add4f0ca77b
Selina <yutong@myvuw.ac.nz>
2021-10-21 01:20:05 +1300

    1.Add Sign up activity. 2.Connect welcome page to sign up page.

--- app/src/main/java/nz/ac/wgtn/mindcare/LoginActivity.java
+++ app/src/main/java/nz/ac/wgtn/mindcare/LoginActivity.java
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Signup extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
     }
 }
--- app/src/main/java/nz/ac/wgtn/mindcare/MainActivity.java
+++ app/src/main/java/nz/ac/wgtn/mindcare/MainActivity.java
@@ -3,20 +3,45 @@ package nz.ac.wgtn.mindcare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignup;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        btnSignup = findViewById(R.id.signupBtn);
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnSignup.setBackgroundColor(Color.BLUE);
+                Intent intent = new Intent(v.getContext(), SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLogin = findViewById(R.id.loginBtn);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnLogin.setBackgroundColor(Color.BLUE);
+                Intent intent = new Intent(v.getContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
     public void signupBtnClick(View view) {
-        Intent intent = new Intent(this, Signup.class);
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
 
