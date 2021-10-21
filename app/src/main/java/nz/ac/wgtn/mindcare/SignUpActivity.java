c6520bcb4a6fb88d965fe9887b4bb6c1963ac83f
Selina <yutong@myvuw.ac.nz>
2021-10-21 13:23:51 +1300

    Change the "main activity" to "welcome activity".

--- app/src/main/java/nz/ac/wgtn/mindcare/SignUpActivity.java
+++ app/src/main/java/nz/ac/wgtn/mindcare/SignUpActivity.java
@@ -3,12 +3,25 @@ package nz.ac.wgtn.mindcare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private EditText userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        userName = findViewById(R.id.editTextUserNameSingup);
     }
+
+    public void signUpBtnClick(View view) {
+        String uName = userName.getText().toString();
+        //Sign up logic should go here
+    }
+
+
 }
