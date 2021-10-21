380d0ebe5d8f8ad9453715663481a9912e57c58f
Selina <yutong@myvuw.ac.nz>
2021-10-21 21:52:32 +1300

    Add a new method "checkDataEntered" to validate entered data in the sign up form.

--- app/src/main/java/nz/ac/wgtn/mindcare/SignUpActivity.java
+++ app/src/main/java/nz/ac/wgtn/mindcare/SignUpActivity.java
@@ -3,12 +3,19 @@ package nz.ac.wgtn.mindcare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
 
     private EditText userName;
+    private EditText email;
+    private EditText password;
+    private Button btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +23,36 @@ public class SignUpActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_sign_up);
 
-//        userName = findViewById(R.id.editTextUserNameSingup);
+        userName = findViewById(R.id.editTextTextPersonName);
+        email = findViewById(R.id.editTextTextEmailAddress);
+        password = findViewById(R.id.editTextTextPassword);
+        btnSignup = findViewById(R.id.btnregister);
     }
 
     public void signUpBtnClick(View view) {
         String uName = userName.getText().toString();
         //Sign up logic should go here
+        checkDataEntered();
     }
 
+    boolean isValidEmail(EditText text) {
+        CharSequence email = text.getText().toString();
+        return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+    }
 
+    boolean isEmpty(EditText text) {
+        CharSequence str = text.getText().toString();
+        return TextUtils.isEmpty(str);
+    }
+
+    void checkDataEntered() {
+        if (isEmpty(userName)) {
+            Toast t = Toast.makeText(this, "You must enter user name to register!", Toast.LENGTH_SHORT);
+            t.show();
+        }
+
+        if (isValidEmail(email) == false) {
+            email.setError("Enter valid email!");
+        }
+    }
 }
