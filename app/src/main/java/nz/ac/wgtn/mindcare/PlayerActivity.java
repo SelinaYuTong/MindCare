b6127c7fd6aee7c8fe06b7ffaf12f255ad6232d1
shingteha <Tahiti2021>
2021-10-21 22:21:55 +1300

    Finished breathing practice page.

--- app/src/main/java/nz/ac/wgtn/mindcare/PlayerActivity.java
+++ app/src/main/java/nz/ac/wgtn/mindcare/PlayerActivity.java
@@ -2,13 +2,38 @@ package nz.ac.wgtn.mindcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class PlayerActivity extends AppCompatActivity {
+
+    VideoView video;
+    //Paste video url in string below
+    String video_url="https://www.youtube.com/watch?v=aNXKjGFUlMs";
+    ProgressDialog pd;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+        video =(VideoView)findViewById(R.id.video);
+        pd=new ProgressDialog(this);
+        pd.setMessage("Buffering video, please wait");
+        pd.show();
+        Uri uri=Uri.parse(video_url);
+        video.setVideoURI(uri);
+        video.start();
+        video.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                pd.dismiss();
+            }
+        });
+
     }
+
 }
