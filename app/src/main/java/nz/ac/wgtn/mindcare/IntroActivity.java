b81bc7508608fbc4e28dedd3d2ef240a0c8dce71
Selina <yutong@myvuw.ac.nz>
2021-10-23 21:01:13 +1300

    Add second and third page of introduction process.

--- app/src/main/java/nz/ac/wgtn/mindcare/IntroActivity.java
+++ app/src/main/java/nz/ac/wgtn/mindcare/IntroActivity.java
@@ -11,7 +11,7 @@ public class IntroActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro);
+        setContentView(R.layout.activity_intro_1st);
         getSupportActionBar().hide();
     }
 
